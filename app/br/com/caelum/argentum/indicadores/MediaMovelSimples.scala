1f44b3bdc9d5fa3955fc7a309f293d9ab0d0f3c9
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2016-05-29 17:31:44 -0300

    Ajuste de scala

--- app/br/com/caelum/argentum/indicadores/MediaMovelSimples.scala
+++ app/br/com/caelum/argentum/indicadores/MediaMovelSimples.scala
@@ -1,13 +1,17 @@
 package br.com.caelum.argentum.indicadores
 
+
 import br.com.caelum.argentum.modelo.SerieTemporal
+
+import scala.math.BigDecimal.RoundingMode
+
 
 /**
 	* Created by danilo on 27/05/16.
 	*/
 class MediaMovelSimples {
 
-	def calcula(posicao:Int, serie:SerieTemporal) = ((posicao - 2) to posicao )
+	def calcula(posicao:Int, serie:SerieTemporal) = (((posicao - 2) to posicao )
 		.map(index =>  serie.candle(index))
-		.map(c => c.fechamento).sum / 3
+		.map(c => c.fechamento).sum / 3).setScale(2, RoundingMode.HALF_UP)
 }
