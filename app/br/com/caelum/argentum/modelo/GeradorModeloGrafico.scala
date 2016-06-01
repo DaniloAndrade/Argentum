60fdcebc6a52cc652156caca25b5793f64503a3c
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2016-05-31 22:43:56 -0300

    Ajuste da Visualização e Controller

--- app/br/com/caelum/argentum/modelo/GeradorModeloGrafico.scala
+++ app/br/com/caelum/argentum/modelo/GeradorModeloGrafico.scala
@@ -1,18 +1,16 @@
 package br.com.caelum.argentum.modelo
 
 import br.com.caelum.argentum.graficos.LineSerie
-import br.com.caelum.argentum.indicadores.MediaMovelSimples
+import br.com.caelum.argentum.indicadores.{Indicador, MediaMovelSimples}
 import br.com.caelum.argentum.Formatters._
 
 /**
 	* Created by danilo on 28/05/16.
 	*/
 class GeradorModeloGrafico(val serie:SerieTemporal, val inicio:Int, val fim:Int) {
-	def plotaMediaMovelSimples = {
-		val mms = new MediaMovelSimples
-
-		val valores = (inicio to fim).map(indice => mms.calcula(indice,serie)).toList
-		LineSerie("MMS  Fechamento", valores)
+	def plotaIndicador(indicador: Indicador) = {
+		val valores = (inicio to fim).map(indice => indicador.calcula(indice,serie)).toList
+		LineSerie(indicador.toString, valores)
 	}
 
 	def categorias = (inicio to fim) map(inicio => serie.candle(inicio).data) map(_.formatTo("dd/MM/yyyy")) toList
--- app/controllers/ArgentumController.scala
+++ app/controllers/ArgentumController.scala
@@ -4,6 +4,7 @@ import javax.inject.{Inject, Singleton}
 
 import br.com.caelum.argentum.graficos._
 import br.com.caelum.argentum.graficos.Chart._
+import br.com.caelum.argentum.indicadores.{IndicadorFechamento, MediaMovelSimples}
 import br.com.caelum.argentum.modelo._
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.{JsValue, Json, Writes}
@@ -43,19 +44,26 @@ class ArgentumController @Inject()(val messagesApi: MessagesApi) extends Control
 		val candles = CandleFactory.constroiCandles(negociacoes)
 		val serieTemporal = SerieTemporal(candles)
 		val gerador = new GeradorModeloGrafico(serieTemporal,2,serieTemporal.ultimaPosicao)
-		val lineSerie = gerador.plotaMediaMovelSimples
+		val lineSerie = gerador.plotaIndicador(new MediaMovelSimples(3,new IndicadorFechamento))
 		val categorias = gerador.categorias
+		val chart: Chart = chartFactory(lineSerie, categorias)
+		val json = Json.toJson(chart)
+		Ok(json).as("text/json")
+	}
+
+	def chartFactory(lineSerie: LineSerie, categorias: List[String]): Chart = {
 		val xAxis = XAxis(categorias)
-		val yAxis = YAxis(Title("R$",0),List(PlotLineValues(0,2,"#808080")))
+		val yAxis = YAxis(Title("R$", 0), List(PlotLineValues(0, 2, "#808080")))
 		val tooltip = Tooltip("R$")
 		val titulo = Title("Indicadores")
 		val chart = Chart(
 			series = List(lineSerie),
-			xAxis=Option(xAxis),
+			xAxis = Option(xAxis),
 			yAxis = Option(yAxis),
 			title = Option(titulo),
 			tooltip = Option(tooltip))
-		val json = Json.toJson(chart)
-		Ok(json).as("text/json")
+		chart
 	}
+
+	def geraGrafico(indicadorBase:String, media:String) = ???
 }
