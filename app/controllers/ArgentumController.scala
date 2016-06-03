ea6a7ed47cb1a26407a8bf0090590b1a45f8534e
Danilo dos Santos Andrade <danilo.s.andrade@gmail.com>
2016-06-03 11:53:45 -0300

    Adicionado Cache da lista de negociações
    Adicionado cache de negociações para evitar muitas chamadas ao webservice

--- app/controllers/ArgentumController.scala
+++ app/controllers/ArgentumController.scala
@@ -1,15 +1,18 @@
 package controllers
 
 import javax.inject.{Inject, Singleton}
-
+import scala.concurrent.duration._
 import br.com.caelum.argentum.graficos._
 import br.com.caelum.argentum.graficos.Chart._
 import br.com.caelum.argentum.indicadores.{IndicadorFechamento, Indicadores, MediaMovelSimples}
 import br.com.caelum.argentum.modelo._
+import play.api.cache.CacheApi
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.libs.json.{JsValue, Json, Writes}
 import play.api.mvc.{Action, Controller}
 import services.ClienteWebService
+
+import scala.concurrent.duration.Duration
 
 /**
 	* Created by danilo on 25/05/16.
@@ -17,7 +20,7 @@ import services.ClienteWebService
 
 
 @Singleton
-class ArgentumController @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport{
+class ArgentumController @Inject()(val messagesApi: MessagesApi , cache:CacheApi) extends Controller with I18nSupport{
 
 	implicit val implicitNegocicaoWrites = new Writes[Negociacao] {
 		override def writes(o: Negociacao): JsValue = {
@@ -36,11 +39,16 @@ class ArgentumController @Inject()(val messagesApi: MessagesApi) extends Control
 	}
 
 	def listar = Action {
-		Ok(views.html.argentum(ClienteWebService.negociacoes))
+		val negociacoes = getNegociacoes
+		Ok(views.html.argentum(negociacoes))
+	}
+
+	def getNegociacoes: List[Negociacao] = {
+		cache.getOrElse("negociacoes", 5.minutes)(ClienteWebService.negociacoes)
 	}
 
 	def mediaMovelSimples = Action{
-		val negociacoes = ClienteWebService.negociacoes
+		val negociacoes = getNegociacoes
 		val candles = CandleFactory.constroiCandles(negociacoes)
 		val serieTemporal = SerieTemporal(candles)
 		val gerador = new GeradorModeloGrafico(serieTemporal,2,serieTemporal.ultimaPosicao, "Indicadores")
@@ -53,7 +61,7 @@ class ArgentumController @Inject()(val messagesApi: MessagesApi) extends Control
 	def geraGrafico(indicadorBase:String, indicadorMedia:String) = Action {
 		val base = Indicadores.criar(indicadorBase)
 		val media = Indicadores.criarIndicadorComposto(indicadorMedia)(3,base)
-		val negociacoes = ClienteWebService.negociacoes
+		val negociacoes = getNegociacoes
 		val candles = CandleFactory.constroiCandles(negociacoes)
 		val serieTemporal = SerieTemporal(candles)
 		val gerador = new GeradorModeloGrafico(serieTemporal,2,serieTemporal.ultimaPosicao,"Indicadores")
